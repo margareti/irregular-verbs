c89016d61b5247d8fa088fa1594a988f423f2040
Margarita Ignatyeva <margaret.vegan@gmail.com>
2016-07-10 00:06:04 +0300

    add verbs to obj

--- js/script.js
+++ js/script.js
@@ -1,9 +1,130 @@
 (function(){
+var raw = ["be","become","begin","bet","bite","bleed","blow","break","bring","build","burn","buy",
+	           "can","catch","choose","come","cost","cut",
+	           "deal","do","draw","dream","drink","drive",
+	           "eat",
+	           "fall","feel","fight","find","fly","forget","forgive","freeze",
+	           "get","give","go","grow",
+	           "hang","have","hear","hide","hit","hold","hurt","keep",
+	           "know",
+	           "lay","lead","leave","lend","let","light","lose",
+	           "make","mean","meet",
+	           "pay","put",
+	           "read","ride","ring","rise","run",
+
+	           "say","see","sell","send","set","shake","shine","shoot",
+	           "show","shut","sing","sink","sit","sleep", "speak",
+	           ,"spend","spill","split","spoil","spread","stand",
+	           "steal","stick","swim",
+
+	           "take","teach","tell","think","throw",
+	           "understand","upset","wake","wear","win","write"]
+
   var verbs = {
-  	'come': ['come', 'came', 'come'],
-  	'sell': ['sell', 'sold', 'sold'],
-  	'sit': ['sit', 'sat', 'sat'],
-  	'wear': ['wear', 'wore', 'worn'],
+  	'be': ['was/were', 'been'],
+  	'become': ['became', 'become'],
+  	'begin': ['began', 'begun'],
+  	'bet': [ 'bet'],
+  	'bite': ['bit', 'bitten'],
+    'bleed': ['bled'],
+    'blow': ['blew', 'blown'],
+    'break': ['broke', 'broken'],
+    'bring': ['brought'],
+    'build': ['built'],
+    'burn': ['burnt'],
+    'buy': ['bought'],
+
+    'can': ['could'],
+    'catch': ['caught'],
+    'choose': ['chose', 'chosen'],
+  	'come': ['came', 'come'],
+  	'cost': ['cost'],
+  	'cut': ['cut'],
+
+  	'deal': ['dealt'],
+  	'do': ['did', 'done'],
+  	'draw': ['drew', 'drawn'],
+  	'dream': ['dreamt', 'dreamt'],
+    'drink': ['drank', 'drunk'],
+    'drive': ['drove', 'driven'],
+
+    'eat': ['ate', 'eaten'],
+    'fall': ['fell', 'fallen'],
+    'feel': ['felt'],
+    'fight': ['fought'],
+    'find': ['found'],
+    'fly': ['flew', 'flown'],
+    'forget': ['forgot', 'forgotten'],
+    'forgive': ['forgave', 'forgiven'],
+    'freeze': ['froze', 'frozen'],
+
+    'get': ['got', 'gotten'],
+    'give': ['gave', 'given'],
+    'go': ['went', 'gone'],
+    'grow': ['grew', 'grown'],
+
+    'hang': ['hung'],
+    'have': ['had'],
+    'hear': ['heard'],
+    'hide': ['hid', 'hidden'],
+    'hit': ['hit'],
+    'hold': ['held'],
+    'hurt': ['hurt'],
+    'keep': ['kept'],
+    'know': ['knew', 'known'],
+
+    'lay': ['laid'],
+    'lead': ['led'],
+    'leave': ['left'],
+    'lend': ['lent'],
+    'let': ['let'],
+    'light': ['lit'],
+    'lose': ['lost'],
+    'make': ['made'],
+    'mean': ['meant'],
+    'meet': ['met'],
+    'pay': ['paid'],
+    'put': ['put'],
+    'read': ['read'],
+    'ride': ['rode', 'ridden'],
+    'ring': ['rang', 'rung'],
+    'rise': ['rose', 'risen'],
+    'run': ['ran', 'run'],
+
+    'say': ['said'],
+    'see': ['saw', 'seen'],
+  	'sell': ['sold', 'sold'],
+  	'send': ['sent'],
+  	'set': ['set'],
+  	'shake': ['shook', 'shaken'],
+  	'shine': ['shone'],
+  	'shoot': ['shot'],
+  	'show': ['showed', 'shown'],
+  	'shut': ['shut'],
+  	'sing': ['sang'],
+  	'sink': ['sank', 'sunk'],
+  	'sit': ['sat'],
+    'sleep': ['slept'],
+    'speak': ['spoke', 'spoken'],
+    'spend': ['spent'],
+    'spill': ['spilled', 'spilt'],
+    'spoil': ['spoilt'],
+    'spread': ['spread'],
+    'stand': ['stood'],
+    'steal': ['stole', 'stolen'],
+    'stick': ['stuck'],
+    'swim': ['swam', 'swum'],
+    'take': ['took', 'taken'],
+    'teach': ['taught'],
+    'tell': ['told'],
+    'think': ['thought'],
+    'throw': ['threw'],
+    'understand': ['understood'],
+    'upset': ['upset'],
+    'wake': ['woke', 'woken'],
+  	'wear': ['wore', 'worn'],
+  	'win': ['won'],
+  	'write': ['wrote', 'written']
   }
   var submitBtn = document.getElementById('chooseVerbs');
   submitBtn.addEventListener('click', populate);
@@ -63,8 +184,19 @@
     var btnWrap = document.getElementsByClassName('btn-wrap')[0];
 
     headline.innerText = el;
+
     for (; i < 3; i++) {
-    	forms[i].innerText = verbs[el][i];
+    	if (i === 0) {
+    		forms[i].innerText = el;
+    	} else {
+    		if (verbs[el].length === i) {
+    			forms[i].innerText = verbs[el][i - 1];
+    		} else {
+    			forms[i].innerText = verbs[el][i];
+    		}
+    		
+    	}
+    	
     }
     study.removeChild(play);
     
